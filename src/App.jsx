4e6597698c3cbeb0a60c83cae1a5a45fa58b8eba
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-09-30 23:12:46 -0400

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     <div className='App'>
       <h1>Trivia Crack</h1>
       <p>Learn about biology, chemistry, history, and math</p>
+      <p className='Count'>Number of flashcards: 10</p>
       <Flashcard></Flashcard>
     </div>
   )
