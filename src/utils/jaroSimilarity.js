f509514f14cb34f3cee530d85ed7548d92e62da8
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-10-07 18:30:10 -0400

    adding/fixing comments

--- src/utils/jaroSimilarity.js
+++ src/utils/jaroSimilarity.js
@@ -1,20 +1,20 @@
 import { max } from './max';
 
 export function jaroSimilarity(s1, s2) {
-    // calculate matching window
+    // Calculate matching window
     var matchingWindow = Math.floor((max(s1.length,s2.length)/2) - 1);
 
-    // error testing
+    // Error testing
     console.log("matching window: " + matchingWindow);
     console.log("input value: " + s2);
     console.log("answer: " + s1);
 
-    // find matching characters
+    // Find matching characters
     var matchingCharacters = 0;
     var s1MatchedArray = [];
     var s2MatchedArray = [];
 
-    // loop over the correct answer (s1)
+    // Loop over the correct answer (s1)
     for (let i = 0; i < s1.length; i++) {
         var j = i - matchingWindow;
         var upperIndex = i + matchingWindow;
@@ -26,10 +26,11 @@ export function jaroSimilarity(s1, s2) {
             upperIndex = s2.length - 1;
         }
 
+        // Loop over the input (s2), checking for matching characters at current index of the correct answer (s1)
         for (j; j <= upperIndex; j++) {
             if (s1[i] == s2[j]) {
                 matchingCharacters += 1;
-                // add the characters to their respective arrays in the order in which they came
+                // Add the characters to their respective arrays in the order in which they came
                 s1MatchedArray.splice(i, 0, s1[i]);
                 s2MatchedArray.splice(j, 0, s2[j]);
                 break;
@@ -37,7 +38,7 @@ export function jaroSimilarity(s1, s2) {
         }
     }
 
-    // count transpositions
+    // Count transpositions
     var numberIndexesToBeChanged = 0;
     var transpositions = 0;
     for (let i = 0; i < s1MatchedArray.length; i++) {
@@ -49,13 +50,13 @@ export function jaroSimilarity(s1, s2) {
         transpositions = Math.ceil(numberIndexesToBeChanged / 2);
     }
 
-    // calculate jaro similarity: J = 1/3 * ( m/s1 + m/s2 + (m-t)/m )
+    // Calculate jaro similarity: J = 1/3 * ( m/s1 + m/s2 + (m-t)/m )
     var sim = 0;
     if (matchingCharacters != 0) {
         sim = 1/3 * ( (matchingCharacters/s1.length) + (matchingCharacters/s2.length) + ((matchingCharacters - transpositions)/matchingCharacters) );
     }
 
-    // error testing
+    // Error testing
     console.log("indexes to be changed: " + numberIndexesToBeChanged);
     console.log("transpositions: " + transpositions);
     console.log("matching characters: " + matchingCharacters);
--- src/utils/max.js
+++ src/utils/max.js
@@ -1,4 +1,4 @@
 export function max(a, b) {
-    // ternary operator - condition ? expression_if_true : expression_if_false
+    // Ternary operator - condition ? expression_if_true : expression_if_false
     return a > b ? a : b;
 }
