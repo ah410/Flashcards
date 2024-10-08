26994e12cb3c679ea9d8ff0acae1b720ffa1ca02
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-10-07 21:21:50 -0400

    fixed logic error in jaro similarity algorithm, you can't have the same matching character for different ith indices

--- src/utils/jaroSimilarity.js
+++ src/utils/jaroSimilarity.js
@@ -4,7 +4,7 @@ export function jaroSimilarity(s1, s2) {
     // Calculate matching window
     var matchingWindow = Math.floor((max(s1.length,s2.length)/2) - 1);
 
-    // Error testing
+    // Debugging
     console.log("matching window: " + matchingWindow);
     console.log("input value: " + s2);
     console.log("answer: " + s1);
@@ -13,6 +13,7 @@ export function jaroSimilarity(s1, s2) {
     var matchingCharacters = 0;
     var s1MatchedArray = [];
     var s2MatchedArray = [];
+    var matchedCharacterIndices = [];
 
     // Loop over the correct answer (s1)
     for (let i = 0; i < s1.length; i++) {
@@ -28,11 +29,18 @@ export function jaroSimilarity(s1, s2) {
 
         // Loop over the input (s2), checking for matching characters at current index of the correct answer (s1)
         for (j; j <= upperIndex; j++) {
-            if (s1[i] == s2[j]) {
+            if (s1[i] == s2[j] && !matchedCharacterIndices.includes(j)) {
+                // Increase matching characters by 1
                 matchingCharacters += 1;
+
                 // Add the characters to their respective arrays in the order in which they came
                 s1MatchedArray.splice(i, 0, s1[i]);
                 s2MatchedArray.splice(j, 0, s2[j]);
+
+                // Add the jth index to the array of matched indices so you can't use it again
+                matchedCharacterIndices.push(j);
+                
+                // Only one matched character per ith index, so break if a match found
                 break;
             }
         }
@@ -56,9 +64,10 @@ export function jaroSimilarity(s1, s2) {
         sim = 1/3 * ( (matchingCharacters/s1.length) + (matchingCharacters/s2.length) + ((matchingCharacters - transpositions)/matchingCharacters) );
     }
 
-    // Error testing
+    // Debugging
     console.log("indexes to be changed: " + numberIndexesToBeChanged);
     console.log("transpositions: " + transpositions);
     console.log("matching characters: " + matchingCharacters);
+
     return sim;
 }
